b4762418d776ba43c2c6b948717059704b440253
Tyler Swenson <tylerjswenson262@gmail.com>
2026-03-25 22:38:36 -0500

    fix: Issue with min joint angle already being negative
    Caused for the action to always be 120 instead of randomly selecting valid actions.

--- environment/continuous_bittle_environment.py
+++ environment/continuous_bittle_environment.py
@@ -19,16 +19,17 @@ class ContinuousBittleEnvironment(BaseBittleEnvironment[ContinuousEnvironmentPar
         super().__init__(parameters=parameters)
 
         self.action_space = gym.spaces.Box(
-            np.array([-self.params.joint_min] * self.sim.NUM_JOINTS),
+            np.array([self.params.joint_min] * self.sim.NUM_JOINTS),
             np.array([self.params.joint_max] * self.sim.NUM_JOINTS)
         )
 
         self.observation_space = gym.spaces.Dict({
-            'joint_history': gym.spaces.Box(-self.params.joint_min, self.params.joint_max, shape=(self.params.length_joint_history, self.sim.NUM_JOINTS), dtype=np.float32),
+            'joint_history': gym.spaces.Box(self.params.joint_min, self.params.joint_max, shape=(self.params.length_joint_history, self.sim.NUM_JOINTS), dtype=np.float32),
             'gyro': gym.spaces.Box(-np.inf, np.inf, shape=(3,), dtype=np.float32),
             'accel': gym.spaces.Box(-np.inf, np.inf, shape=(3,), dtype=np.float32)
         })
 
 
     def decode_action(self, action):
+        return action
         return np.deg2rad(action)
