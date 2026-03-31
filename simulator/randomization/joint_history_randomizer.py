2078da5adcfcc1490346d0e3a24fc06f50615710
Tyler Swenson <tylerjswenson262@gmail.com>
2026-03-31 04:59:21 -0500

    fix: Fixed error with the noise scale being applied to the joint history

--- simulator/randomization/joint_history_randomizer.py
+++ simulator/randomization/joint_history_randomizer.py
@@ -2,21 +2,26 @@ from simulator.physics.core import RandomizationSubsystem
 import numpy as np
 
 class JointHistoryRandomizer(RandomizationSubsystem):
-    def __init__(self, sim, noise=(-15, 15)):
+    def __init__(self, sim, noise_scale=5):
         super().__init__(sim)
 
-        self.noise = noise
+        self.noise_scale = noise_scale
+
+    @property
+    def size(self):
+        length_history, num_joints = self.sim.sim_state.joints.size()
+        return length_history, num_joints
+
 
     def apply(self, rng):
-        length_history, num_joints = self.sim.sim_state.joints.size()
-        joint_history = np.empty((length_history, num_joints))
+        length_history, num_joints = self.size
+        joint_history = np.zeros((length_history, num_joints), dtype=np.float32)
         
-
         joint_angles = self.context.kinematics.joint.get_angles()
         joint_history[0] = joint_angles
 
-        noise = rng.normal(0, 0.01, size=(length_history - 1, num_joints))
+        noise = rng.normal(0, self.noise_scale, size=(length_history - 1, num_joints))
         joint_history[1:] = joint_angles + noise
 
-        self.sim.sim_state.joints.set_history(joint_history.astype(int))
+        self.sim.sim_state.joints.set_history(joint_history)
 
