71e6555e5b127db2122ec72c7b8f3d4f71fc232e
Tyler Swenson <tylerjswenson262@gmail.com>
2026-04-06 16:13:49 -0500

    chore: Adjusted joint initial position

--- simulator/modules/randomization/joint_randomizer.py
+++ simulator/modules/randomization/joint_randomizer.py
@@ -12,6 +12,9 @@ import numpy as np
 
 @register_module(Randomization, noise=(-15, 15))
 class JointRandomizer(Subsystem):
+    #_initial_position = [1, 0, 1, 0, 1, 0, 1, 0]
+    _initial_position = [0.5, 0] * 4
+
     def __init__(self, sim, noise):
         super().__init__(sim)
         self.joint_qpos_ids = None
@@ -21,12 +24,15 @@ class JointRandomizer(Subsystem):
         self.joint_qpos_ids = self.sim.robot_info.joint_qpos_ids
 
     def reset_start(self, rng):
+        
+        
         random_joint_angles_deg = rng.uniform(
             *self.noise,
             size=len(self.joint_qpos_ids)
         )
         
         kn_joint = self.sim.get(Physics).get(Kinematics).get(JointKinematics)
-        kn_joint.set_angles(np.deg2rad(random_joint_angles_deg))
+        kn_joint.set_angles(self._initial_position + np.deg2rad(random_joint_angles_deg))
+        #kn_joint.set_angles(np.deg2rad(random_joint_angles_deg))
 
 
